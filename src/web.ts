b1109e8f727aec3acc79f93f9dfe9a8eee2d3cc5
imba97 <mail@imba97.cn>
2025-02-25 17:57:14 +0800

    style: update types

--- src/web.ts
+++ src/web.ts
@@ -3,15 +3,15 @@ import { WebPlugin } from '@capacitor/core';
 import type { AudioStreamPlugin } from './definitions';
 
 export class AudioStreamWeb extends WebPlugin implements AudioStreamPlugin {
-  async load(): Promise<any> {
+  async load(): Promise<void> {
     throw new Error('Method not implemented.');
   }
 
-  async start(): Promise<any> {
+  async start(): Promise<void> {
     throw new Error('Method not implemented.');
   }
 
-  async stop(): Promise<any> {
+  async stop(): Promise<void> {
     throw new Error('Method not implemented.');
   }
 }
